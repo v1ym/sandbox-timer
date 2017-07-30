fa08e3f49f90340b4df64d09aad9bae540c3f0b8
v1ym <jpbarker2@gmail.com>
2017-07-30 11:18:57 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
+
 var counting = false;
 
 timer = 150000;
 
 function start() {
   counting = true;
-  console.log(counting);
 };
 
 function stop() {
   counting = false;
-  console.log(counting);
 };
 
-
-  var countdown = setInterval(tick, 1000);
-  function tick() {
-    if (counting) {
-      timer -= 1000;
-      var min = Math.floor(timer % (1000*60*60)/(1000*60));
-      var seconds = Math.floor(timer % (1000*60)/(1000));
-      document.title = min + " : " + seconds;
-      document.getElementById('banner').innerHTML = min + "m " + seconds + "s";
-};
+var countdown = setInterval(tick, 1000);
+function tick() {
+  if (counting) {
+    timer -= 1000;
+    var min = Math.floor(timer % (1000*60*60)/(1000*60));
+    var seconds = Math.floor(timer % (1000*60)/(1000));
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    };
+    document.title = min + " : " + seconds;
+    document.getElementById('banner').innerHTML = min + "m " + seconds + "s";
+  };
 
 };
